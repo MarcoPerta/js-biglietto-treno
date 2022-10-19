6d687dc18e721e9c05cfcceafb44b34ef2804cc7
MarcoPerta <perta.marco@gmail.com>
2022-10-19 15:46:25 +0200

    form e recupero dati dal form

---
+++ main.js
@@ -0,0 +1,6 @@
+function MakeThePrice(){
+    let Km= document.getElementById('HowmanyKm').value;
+    console.log(`Km`)
+    let Age = document.getElementById('HowOld').value;
+    console.log(`Age`)
+} 

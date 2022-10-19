0c5ef05890adaf8cbd5e25445a6236443f422bc0
MarcoPerta <perta.marco@gmail.com>
2022-10-19 16:12:30 +0200

    cerco di calcolare prezzo

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 function MakeThePrice(){
-    let Km= document.getElementById('HowmanyKm').value;
-    console.log(`Km`)
+    let Km = document.getElementById('HowmanyKm').value;
+    console.log(Km)
     let Age = document.getElementById('HowOld').value;
-    console.log(`Age`)
+    console.log(Age)
+    
+    let priceKm = `0.21 * Km`
+
+    document.getElementById('Price').innerHtml = `Il prezzo è : priceKm euro`
 } 
